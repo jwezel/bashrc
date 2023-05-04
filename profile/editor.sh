f4c143b50f50ab33b7fb8bffe1c0652b2f17ee36
Johnny Wezel <j@wezel.name>
2023-05-04 06:16:26 +0200

    editor.sh

--- profile/editor.sh
+++ profile/editor.sh
@@ -1,2 +1,3 @@
 export EDITOR=$(which mcedit || which joe || which nedit || which vi || which vim || echo /dev/null)
-export VISUAL=$(which sublw || which subl || which sublime_text || echo $EDITOR)
+export VISUAL=$((which sublw && echo sublw --wait) || (which subl && echo subl --wait) || (which sublime_text && echo sublime_text --wait) || echo $EDITOR)
+alias v="$EDITOR"
