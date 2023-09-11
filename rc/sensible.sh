d8e25ba80bf39f7a1b9d09b985bc128cd3291c96
Johnny Wezel <j@wezel.name>
2023-09-11 21:29:49 +0200

    sensible bash: HISTIGNORE

--- rc/sensible.sh
+++ rc/sensible.sh
@@ -66,7 +66,7 @@ HISTFILESIZE=100000
 HISTCONTROL="erasedups:ignoreboth"
 
 # Don't record some commands
-export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+export HISTIGNORE="&:[ ]*:exit:reset:pwd:bg:fg:history:hh:hstr:clear"
 
 # Use standard ISO 8601 timestamp
 # %F equivalent to %Y-%m-%d
