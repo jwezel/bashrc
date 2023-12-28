569fccd95c2b6bdf975838705c1398d9a61dd8d4
Johnny Wezel <j@wezel.name>
2023-12-28 22:51:15 +0100

    editor.sh

--- profile/editor.sh
+++ profile/editor.sh
@@ -1,2 +1,2 @@
-export EDITOR="$(which mcedit || which joe || which nano || which vi || which vim || echo /dev/null)"
-export VISUAL="$( (which sublw && echo --wait) || (which subl && echo --wait) || (which sublime_text && echo --wait) || echo $EDITOR)"
+export EDITOR="$(which micro || which mcedit || which joe || which nano || which vi || which vim || echo /dev/null)"
+export VISUAL="$( (which sublww) || (which subl) || (which sublime_text) || echo $EDITOR)"
