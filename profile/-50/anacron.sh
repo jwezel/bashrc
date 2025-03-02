eda96cafc6e5c22d038b02a96fcfd4ed0ad6cc27
Johnny Wezel <j@wezel.name>
2025-03-02 12:54:13 +0100

    Anacron

--- profile/-50/anacron.sh
+++ profile/-50/anacron.sh
@@ -1,6 +1,4 @@
-set -x
 if [ -n "$(type -p anacron)" -a -e "$HOME/.local/etc/anacrontab" ]; then
   mkdir -vp "$HOME/.local/var/anacron-spool"
   anacron -t "$HOME/.local/etc/anacrontab" -S "$HOME/.local/var/anacron-spool" 'profile-*'
 fi
-set +x
