7678129b13f3cedf268d7903fed1e31cc79d3d05
Johnny Wezel <j@wezel.name>
2025-04-20 19:35:41 +0200

    Changed tty/kitty.sh

--- tty/kitty.sh
+++ tty/kitty.sh
@@ -1,3 +1,4 @@
-if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then
-	source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"
+if test -n "$KITTY_INSTALLATION_DIR"; then
+    export KITTY_SHELL_INTEGRATION="enabled"
+    source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"
 fi
